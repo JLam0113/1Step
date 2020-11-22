949446d5f9c795c97d5e23bc28f8fb389e560722
GodPlue <43499445+GodPlue@users.noreply.github.com>
2020-11-22 18:29:14 -0500

    Revert "Merge branch 'main' into main2"
    This reverts commit cb5a2813287f748b51837e7cf6a264fc8c9f3eb9, reversing
    changes made to c9187f17571ad9418a42355397114d83ce0b966c.

--- app/src/main/java/com/example/a1step/UserSettings.java
+++ app/src/main/java/com/example/a1step/UserSettings.java
@@ -5,6 +5,8 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.sql.Date;
+
 @Entity(tableName = "user_settings")
 public class UserSettings {
 
